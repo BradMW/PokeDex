e9b22231a2c46f40c879ce780d2e4728bb9779ab
BradMW <bodell94@yahoo.com>
2022-03-30 21:14:55 -0500

    created a list and card file. next step is to get the API

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from "./components/layout/NavBar"
+import Dashboard from './components/layout/Dashboard';
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
-     
+      <div className='container'>
+        <Dashboard />
+      </div>
     </div>
   );
 }
