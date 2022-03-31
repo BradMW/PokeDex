93103e4b331e08c727edd116f56a037ea8fcfb60
BradMW <bodell94@yahoo.com>
2022-03-30 20:59:23 -0500

    added a few folders. installed bootstrap

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,9 +1,12 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import NavBar from "./components/layout/NavBar"
 
 function App() {
   return (
     <div className="App">
-     <h1>Welcome to my pokedex!</h1>
+     <NavBar/>
      
     </div>
   );
